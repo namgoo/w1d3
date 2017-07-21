008d724bd7157825fb219ce14ed9aa3051e4a7a6
Nam Yoo <namgooy@gmail.com>
2017-07-21 19:15:17 +0000

    Finally finished sales-total-tax

--- sales-total-tax.js
+++ sales-total-tax.js
@@ -26,35 +26,99 @@ var companySalesData = [
 ];
 
 
+/* Expected Results:
+{
+  Telus: {
+    totalSales: 1300
+    totalTaxes: 144
+  },
+  Bombardier: {
+    totalSales: 800,
+    totalTaxes: 40
+  }
+}
+*/
+
 
 function calculateSalesTax(salesData, taxRates) {
   // Implement your code here
+  var result = {};
+  var addSales = 0;
+  var addTax = 0;
+  // var totalSales = 0;
+  // var totalTaxes = 0;
+  console.log(taxRates)
+  salesData.forEach(function (salesData) {
+    for (j = 0; j < salesData.sales.length; j++) {
+       addSales += salesData.sales[j]
+    }
+    salesData['addSales'] = addSales
+    addSales = 0
+
+    pr = (salesData.province)
+
+    addTax = (taxRates[pr] * salesData['addSales'])
+    salesData['addTax'] = addTax
+  })
+
+  console.log("salesData: ", salesData)
+
+  salesData.forEach(function (salesData) {
+      if (!result[salesData['name']]){
+        result[salesData['name']] = { totalSales: 0 , totalTaxes: 0}
+      }
+  })
+  console.log("result", result)
+
+  // salesData.forEach(function (salesData) {
+
+  var keys = Object.keys(result)
+  console.log(keys)
+  for (var m = 0; m < keys.length; m++) {
+    for (var n = 0; n < salesData.length; n++) {
+      if (salesData[n]['name'] === keys[m]) {
+        console.log("keys[m]" , keys[m])
+        console.log("salesData[n]['addSales']" , salesData[n]['addSales'])
+        console.log("salesData[n]['addTax']" , salesData[n]['addTax'])
+
+        result[keys[m]].totalSales += salesData[n]['addSales']
+        result[keys[m]].totalTaxes += salesData[n]['addTax']
+
+        // result[keys[m]] = {'totalSales': totalSales , 'totalTaxes' : totalTaxes }
+      }
+
+      console.log("result during each loop", result)
+    }
+  }
+
+
 }
 
-var results = calculateSalesTax(companySalesData, salesTaxRates);
+ console.log(calculateSalesTax(companySalesData, salesTaxRates));
+
+
+
+
+
+
+
+
+
+
 
 
 
 
 // sum = 0
-company = []
+// company = []
 
-for (var i = 0; i < companySalesData.length; i ++) {
-    if (companySalesData[i]['name'])
-    company.push(companySalesData[i]['name'])
+// for (var i = 0; i < companySalesData.length; i ++) {
+//     if (companySalesData[i]['name'])
+//     company.push(companySalesData[i]['name'])
 
-console.log(company)
+// console.log(company)
 //     for (var j = 0; j < companySalesData[i]['sales'].length; j ++)
 //     sum = companySalesData[i]['sales'][j]
-
-
-
-
-
-
-
-
-
 
 
 /* Expected Results:
@@ -69,5 +133,11 @@ console.log(company)
   }
 }
 */
+
+
+
+
+
+
 
 
